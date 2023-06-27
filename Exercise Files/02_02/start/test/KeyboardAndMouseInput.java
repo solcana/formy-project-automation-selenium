5cb54dd066a8338020502d44eb4620eaf1fe1cd6
Ana Solcan <solcan.anna@gmail.com>
2023-06-27 11:52:17 +0200

    Initial commit

---
+++ Exercise Files/02_02/start/test/KeyboardAndMouseInput.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class KeyboardAndMouseInput {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/anasolcan/Downloads/chromedriver_mac64/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/keypress");
+
+        WebElement name = driver.findElement(By.id("name"));
+        name.click();
+        name.sendKeys("Ana Solcan");
+
+        WebElement button = driver.findElement(By.id("button"));
+        button.click();
+
+
+        driver.quit();
+    }
+}
---
+++ Exercise Files/02_03/start/test/Autocomplete.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/anasolcan/Downloads/chromedriver_mac64/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+}
